e289a98a95a412170c0a13c5061647146192d01b
sergivi11 <sergivi04@gmail.com>
2022-10-04 13:42:18 +0200

    MaskifyJava

--- src/main/java/org/example/SevenKyu/Maskify.java
+++ src/main/java/org/example/SevenKyu/Maskify.java
@@ -3,11 +3,10 @@ package org.example.SevenKyu;
 public class Maskify {
     public static String maskify(String str) {
         char[] stringChar = str.toCharArray();
-        if (stringChar.length > 4){
-            for (int i = 0; i < stringChar.length - 4; i++){
-                stringChar[i] = '#';
-            }
+        for (int i = 0; i < stringChar.length - 4; i++){
+            stringChar[i] = '#';
         }
+
         return String.valueOf(stringChar);
     }
 }
