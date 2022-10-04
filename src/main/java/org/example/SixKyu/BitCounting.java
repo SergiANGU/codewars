fc7f17dc51b926028aa3b72cd1d5225eafd2bc18
sergivi11 <sergivi04@gmail.com>
2022-10-04 13:38:01 +0200

    MaskifyJava

--- src/main/java/org/example/SixKyu/BitCounting.java
+++ src/main/java/org/example/SixKyu/BitCounting.java
@@ -1,12 +1,8 @@
 package org.example.SixKyu;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class BitCounting {
 
-    public static int countBits(int n){
+    public static int countBits(int n) {
         return Integer.bitCount(n);
     }
 }
